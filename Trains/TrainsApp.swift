dedc041c566ebaba3dfebb27a19fdbbd8b2429f6
Vsevolod Oplachko <oplachko2003@gmail.com>
2026-09-07 16:12:11 +0500

    Initial Commit

---
+++ Trains/TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsApp.swift
+//  Trains
+//
+//  Created by Vsevolod Oplachko on 07.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
